f94c882caf05cf074c22ab8c0c97c221fa17bd6a
amol4anoff <amolchanov@optimal-dynamics.com>
2018-02-20 11:49:30 +0600

    first commit

---
+++ Sikuli/SSH.Mac(Chrome).sikuli/SSH.Mac(Chrome).py
@@ -0,0 +1,88 @@
+click(Pattern("1518860545657.png").targetOffset(0,2))
+wait(4)
+type(Pattern("1518862042812.png").targetOffset(1,1), "cd .ssh" + Key.ENTER)
+wait(2)
+type(Pattern("1518862391544.png").targetOffset(1,-1), "ssh-keygen -t rsa" + Key.ENTER)
+wait(2)
+type(Pattern("1518862558597.png").targetOffset(172,-1), "anton" + Key.ENTER)
+wait(2)
+type(Pattern("1518862625558.png").targetOffset(79,-1), "anton" + Key.ENTER)
+wait(1)
+type(Pattern("1518862692834.png").targetOffset(39,0), "anton" + Key.ENTER)
+wait(2)
+type(Pattern("1518863133634.png").targetOffset(-1,0), "cd" + Key.ENTER)
+wait(2)
+type(Pattern("1518863235707.png").targetOffset(-5,1), "open .ssh" + Key.ENTER)
+wait(2)
+rightClick(Pattern("1518863753876.png").targetOffset(0,-7))
+wait(3)
+hover(Pattern("1518863827497.png").targetOffset(-56,0))
+wait(3)
+click(Pattern("1518863936095.png").targetOffset(-42,0))
+wait(3)
+type(Pattern("1518871567228.png").targetOffset(-30,1), "textedit")
+wait(4)
+doubleClick(Pattern("1518871620768.png").targetOffset(-5,5))
+wait(3)
+type(Pattern("1518871250548.png").targetOffset(1,9), "a" ,KEY_CMD)
+wait(2)
+type(Pattern("1518871274455.png").targetOffset(1,9), "c" ,KEY_CMD)
+wait(4)
+App.open("Google Chrome.app")
+wait(3)
+type(Pattern("1518865781219.png").targetOffset(-166,0), "https://masterhub.subut.ai/login" + Key.ENTER)
+wait(2)
+type(Pattern("1518865981122.png").targetOffset(-107,0), "ant.silent@mail.ru")
+wait(1)
+click(Pattern("1518866161480.png").targetOffset(-80,2))
+wait(2)
+type(Pattern("1518866018540.png").targetOffset(-100,0), "Ab123456")
+wait(3)
+click(Pattern("1518866057680.png").targetOffset(-5,2))
+wait(3)
+click(Pattern("1518866242880.png").targetOffset(3,4))
+wait(3)
+click(Pattern("1518866281861.png").targetOffset(-28,3))
+wait(3)
+click(Pattern("1518866342758.png").targetOffset(-1,3))
+wait(2)
+type(Pattern("1518867062462.png").targetOffset(-109,-65), "v" ,KEY_CMD)
+wait(2)
+click(Pattern("1518867164975.png").targetOffset(0,1))
+wait(3)
+click("1518867512029.png")
+wait(8)
+click(Pattern("1518867567942.png").targetOffset(1,3))
+wait(2)
+click(Pattern("1518867848588.png").targetOffset(0,4))
+wait(2)
+click(Pattern("1518867890675.png").targetOffset(0,1))
+wait(6)
+click(Pattern("1518867957759.png").targetOffset(-1,1))
+wait(2)
+click(Pattern("1518867984546.png").targetOffset(1,1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sikuli/Tray+P2P.Debian(Chrome).sikuli/Tray+P2P.Debian(Chrome).py
@@ -0,0 +1,101 @@
+App.open("google-chrome")
+wait(2)
+type(Pattern("1518953630674.png").targetOffset(-156,0), "https://masterhub.subut.ai/login" + Key.ENTER)
+wait(2)
+type(Pattern("1518953837699.png").targetOffset(-98,1), "ant.silent@mail.ru")
+wait(2)
+type(Pattern("1518953878538.png").targetOffset(-105,-1), "Ab123456")
+click(Pattern("1518953981335.png").targetOffset(-5,3))
+wait(7)
+click(Pattern("1518954026534.png").targetOffset(0,-1))
+hover(Pattern("1518954059661.png").targetOffset(-83,-2))
+wait(2)
+click(Pattern("1518954178723.png").targetOffset(-22,1))
+wait(3)
+type(Key.PAGE_DOWN)
+wait(2)
+click(Pattern("1518954259177.png").targetOffset(-22,1))
+wait(5)
+type(Pattern("1518954309552.png").targetOffset(-87,2), "subutai e2e" + Key.ENTER)
+wait(2)
+click(Pattern("1518954373766.png").targetOffset(-20,2))
+wait(3)
+click(Pattern("1518954410958.png").targetOffset(-5,3))
+wait(5)
+type(Pattern("1518954443630.png").targetOffset(-64,1), "ant.silent@mail.ru")
+wait(3)
+click(Pattern("1518954526356.png").targetOffset(-3,2))
+wait(8)
+click(Pattern("1518954599393.png").targetOffset(-4,1))
+wait(3)
+type(Pattern("1518954636096.png").targetOffset(-156,-3), "https://subutai.io/install/index.html" + Key.ENTER)
+wait(2)
+type(Key.PAGE_DOWN)
+wait(2)
+click(Pattern("1518954937762.png").targetOffset(-50,-3))
+wait(2)
+click(Pattern("1518955002218.png").targetOffset(-26,2))
+wait(10)
+click(Pattern("1518955054087.png").targetOffset(24,1))
+wait(2)
+click(Pattern("1518955084590.png").targetOffset(-4,3))
+wait(60)
+click(Pattern("1518955167165.png").targetOffset(24,-4))
+wait(2)
+App.open("gnome-terminal")
+type(Pattern("1518955457496.png").targetOffset(-65,2), "cd Downloads" + Key.ENTER)
+wait(2)
+type(Pattern("1518955547813.png").targetOffset(-40,-1), "sudo dpkg -i subutai-p2p-master.deb" + Key.ENTER)
+wait(2)
+type(Pattern("1518955771791.png").targetOffset(26,0), "debian")
+wait(7)
+type(Pattern("1518955914787.png").targetOffset(-227,0), "sudo dpkg -i subutai-p2p-master.deb" + Key.ENTER)
+wait(8)
+type(Pattern("1518955914787.png").targetOffset(-204,-2), "SubutaiTray" + Key.ENTER)
+wait(8)
+type(Pattern("1518956222794.png").targetOffset(-106,4), "ant.silent@mail.ru")
+wait(2)
+type(Pattern("1518956316134.png").targetOffset(-83,1), "Ab123456")
+wait(2)
+click(Pattern("1518956384956.png").targetOffset(-12,0))
+wait(10)
+click(Pattern("1518957048131.png").targetOffset(-13,35))
+wait(2)
+rightClick(Pattern("1518957083004.png").targetOffset(6,28))
+wait(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+            
+
+
+
+
+
+
+
+
+
